ca0e6ab673d4760bfa4403754946a4f7b4f1c9ba
lkov <lynseykovar@gmail.com>
2019-08-14 11:14:11 -0400

    fix names of functions

--- my-seq-utils/gene_hits.py
+++ my-seq-utils/gene_hits.py
@@ -1,4 +1,5 @@
 #read bam file and list of regions, output regions with hits, percentage of regions with hits
+# usage: python gene_hits.py bamfile bedfile
 
 from sys import argv
 import pysam
@@ -9,28 +10,29 @@ script, bam, bed = argv
 
 def main():
     bedstem = bed.replace('.bed', '')
+    bamstem = bam.replace('.bam', '')
     bamfile = pysam.AlignmentFile(bam, 'rb')
     regions = pd.read_csv(bed, delimiter = "\t", header = None)
 
     regions['coverage'] = np.vectorize(getcov)(bamfile, regions[0], regions[1], regions[2])
 
-    print(f"Writing bed file with coverage to: {bedstem + '.coverage.bed'}")
-    writecov(bedstem + 'coverage.bed', regions)
+    print(f"Writing bed file with coverage to: {bamstem + '.coverage.bed'}")
+    writecov(bamstem + 'coverage.bed', regions)
 
     print(f"Proportion of regions with hits: {covperc(regions)}")
 
 
 # returns number of reads that aligned to the specified region
-def get_cov(df, chr, sta, sto):
+def getcov(df, chr, sta, sto):
     return df.count(contig = chr, start = sta, stop = sto)
 
 # calculates the proportion of regions with hits
-def get_cov_perc(df):
+def covperc(df):
     expr_regions = len(df[(df['coverage'] > 0)])
     return expr_regions / len(df)
 
 # writes bedfile with coverage information to a tsv file
-def write_cov(fname, df):
+def writecov(fname, df):
     df = df[[0, 1, 2, 'coverage']]
     df.columns = ['chrom', 'start', 'stop', 'coverage']
     df.to_csv(fname, sep = "\t", index = False)
